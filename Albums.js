7120875fe3e7aac709163f73622cfca20ac97326
Emmanuel Awed <emmanuelawed1@gmail.com>
2017-09-11 19:45:41 -0400

    -initializing

---
+++ Albums.js
@@ -0,0 +1,9 @@
+var albums = ["holyRolling", "loveTrain", "protoolsCallsofZion"]
+
+ holyRolling {
+    name = "Holy Rolling";
+    emcee = "California Ghost King";
+    realesingDay= "26th of July";
+    price = 20;
+}
+
